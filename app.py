760aafeffed2af435a6ec0849038a811359e60d6
Armando Zubillaga <ajzpiv97@gmail.com>
2022-11-09 10:59:02 -0500

    Modified logging

--- app.py
+++ app.py
@@ -2,35 +2,19 @@ import logging
 import os
 from flask import Flask, request
 from flask_cors import CORS
-from datetime import datetime as dt
 from flaskr.middleware.middleware import ContentTypeMiddleware
 from flaskr.routes import auth
 from flaskr.utils.custom_response import CustomResponseHandler
 from flaskr.utils.error_handler import error_handler
 
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': "[%(asctime)s] [p%(process)s] [%(levelname)s] {%(filename)s:"
-                  "%(lineno)d} %(message)s",
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
-
 app = Flask(__name__, instance_relative_config=True)
 
 
 def create_app():
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
     testing = os.environ.get('TESTING', False)
     app.config['TESTING'] = testing
     app.response_class = CustomResponseHandler
