164bdea524fa31b6b5543483e29c19b4a208279f
Armando Zubillaga <ajzpiv97@gmail.com>
2022-11-21 11:05:28 -0500

    Added kill command

--- bin/run_dev_server.sh
+++ bin/run_dev_server.sh
@@ -3,7 +3,7 @@ export SETTING=dev
 export DEV_DATABASE_URL=postgres://snemjqmwzxqtgy:b2d4c7f51b6ae77dc64ce67d05ec1fefeed4b4ee3d397f9add2047525283de36@ec2-18-215-41-121.compute-1.amazonaws.com:5432/dpfbv163nkor3
 
 if [[ -z "$SECRET_KEY" ]]; then
-  echo "$SECRET_KEY" is empty
+  echo SECRET_KEY is empty
   exit 1
 
 fi
@@ -11,6 +11,16 @@ fi
 echo "SECRET_KEY=$SECRET_KEY"
 echo "SETTING=$SETTING"
 echo "DEV_DATABASE_URL=$DEV_DATABASE_URL"
+
+pids=$(ps -ef | grep gunicorn | grep -v "grep" | awk '{print $2}')
+
+if [ -z "$pids" ]; then
+  echo "No pids found"
+else
+  echo "pids=$pids"
+  kill -9 $pids
+
+fi
 
 gunicorn main:app --reload --log-level=info
 
