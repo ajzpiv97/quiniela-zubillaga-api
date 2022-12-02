0db209e1e5c2d8d470980ee8bd5e44fca393206a
Armando Zubillaga <ajzpiv97@gmail.com>
2022-12-01 23:28:39 -0500

    Added command to add new rounds

--- flaskr/utils/commands.py
+++ flaskr/utils/commands.py
@@ -3,9 +3,11 @@ import logging
 import pandas as pd
 from flask.cli import with_appcontext
 from flaskr.db.games import Games
+from flaskr.db.rounds import Rounds
 from flaskr.utils.extensions import db
 from flaskr.utils.local_utils import update_user_points_based_on_predictions
-from flaskr.utils.local_utils import get_timestamp
+from flaskr.utils.local_utils import get_datetime_object_from_pattern
+
 logger = logging.getLogger(__name__)
 
 
@@ -30,16 +32,47 @@ def init_games(file):
     print(data.shape[0])
     for i in range(data.shape[0]):
         try:
+            date = data.iloc[i]["date"]
+            time = data.iloc[i]["time"]
+            utc_timestamp = get_datetime_object_from_pattern(date, time).timestamp()
             new_game = Games(team_a=data.iloc[i]['equipo1'].strip().upper(),
                              team_b=data.iloc[i]['equipo2'].strip().upper(),
-                             date=data.iloc[i]['fecha'],
-                             group=data.iloc[i]['grupo'])
+                             match_date=utc_timestamp,
+                             match_group=data.iloc[i]['grupo'])
             new_game.save()
             logger.info('Game successfully created!' + data.iloc[i]['equipo1'] + " VS " + data.iloc[i]['equipo2'])
             print('Game successfully created!' + data.iloc[i]['equipo1'] + " VS " + data.iloc[i]['equipo2'])
         except Exception as e:
             print(e)
             print("error logging games")
+
+
+@click.command(name="init_rounds")
+@click.option('--file')
+@with_appcontext
+def init_rounds(file):
+    data = pd.read_csv(file)
+    print(data.shape[0])
+    for i in range(data.shape[0]):
+        try:
+            start_date = data.iloc[i]["start_date"]
+            start_time = data.iloc[i]["start_time"]
+            end_date = data.iloc[i]["end_date"]
+            end_time = data.iloc[i]["end_time"]
+            start_utc_object = get_datetime_object_from_pattern(start_date, start_time)
+            end_utc_object = get_datetime_object_from_pattern(end_date, end_time)
+
+            new_round = Rounds(round_name=data.iloc[i]['round'].strip().upper(),
+                               round_start_datetime=start_utc_object.strftime("%Y-%m-%d %H:%M:%S"),
+                               round_start_timestamp=start_utc_object.timestamp(),
+                               round_end_datetime=end_utc_object.strftime("%Y-%m-%d %H:%M:%S"),
+                               round_end_timestamp=end_utc_object.timestamp())
+            new_round.save()
+            logger.info('Round successfully created!' + data.iloc[i]['round'])
+            print('Round successfully created!' + data.iloc[i]['round'])
+        except Exception as e:
+            print(e)
+            print("error logging round")
 
 
 @click.command(name="update_game_time")
@@ -54,7 +87,7 @@ def update_game_time(file):
                                                   match_group=data.iloc[i]['grupo']).first()
             date = data.iloc[i]["date"]
             time = data.iloc[i]["time"]
-            utc_timestamp = get_timestamp(date, time)
+            utc_timestamp = get_datetime_object_from_pattern(date, time).timestamp()
             temp_game_obj.update(match_date=utc_timestamp)
             temp_game_obj.save()
             logger.info('Game successfully updated!' + data.iloc[i]['date'])
