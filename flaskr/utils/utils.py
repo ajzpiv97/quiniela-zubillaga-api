6aaf0acd7179907e3cf0fb8c3ad1d1b7c02c895f
Armando Zubillaga <ajzpiv97@gmail.com>
2022-12-01 23:27:42 -0500

    Modified string

--- flaskr/utils/utils.py
+++ flaskr/utils/utils.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 def modify_database_url_to_add_dialect() -> str:
     try:
         semi_colon_idx = DATABASE_URL.find(':')
-        sql_alchemy_dialect = DATABASE_URL[0:semi_colon_idx] + 'ql+psycopg2'
+        sql_alchemy_dialect = 'postgresql+psycopg2'
         new_connection_url = sql_alchemy_dialect + DATABASE_URL[semi_colon_idx::]
         return new_connection_url
 
